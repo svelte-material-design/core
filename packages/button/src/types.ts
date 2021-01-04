889a22b3eaaa2f8760ae6789b8aa6f57b2e20518
Ray Thurne Void <monkeythedev@gmail.com>
2021-01-04 09:32:03 +0100

    @svelte-material-design/core/button exports icon type and dom

--- packages/button/src/types.ts
+++ packages/button/src/types.ts
@@ -1,5 +1,8 @@
 import type { A, Button } from "../../common/dom";
 
+export type { GraphicType as IconType } from "../../common/components";
+export type { GraphicDOM as IconDOM } from "../../common/components";
+
 export type ButtonComponent = typeof Button | typeof A;
 export type ButtonVariant = "text" | "raised" | "unelevated" | "outlined";
 export type ButtonColor = "primary" | "secondary";
