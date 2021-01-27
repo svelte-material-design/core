7610d4920430eb930b07439046f8e1f64f939e27
Ray Thurne Void <raythurnevoid@gmail.com>
2021-01-27 06:43:29 +0100

    @svelte-material-design/slider enhancements

--- packages/slider/src/index.ts
+++ packages/slider/src/index.ts
@@ -1,4 +1,4 @@
-export { default as ContinuousSlider } from "./ContinuousSlider.svelte";
+export { default as Slider } from "./Slider.svelte";
 export { default as DiscreteSlider } from "./DiscreteSlider.svelte";
 export { default as Range } from "./Range.svelte";
 export { default as Thumb } from "./Thumb.svelte";
